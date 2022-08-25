bb6835299b32b166ac7e62d80cd9a726c5c5cae1
Astrid Hecht <silentaviary@gmail.com>
2022-08-24 23:32:08 -0600

    Write basic view documents for artists, items, and atrist_items so all tests are passed

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -10,10 +10,15 @@ RSpec.describe 'Item#index' do
   describe 'as a visitor' do
     describe 'when I visit "/items"' do
       before :each do
+        @artist_1 = Artist.create!(username: 'UnnecessaryilyResin', location: 'Laramie, Wyoming', followers: 157,
+                                   accepts_returns: false)
+        @artist_2 = Artist.create!(username: 'PetHats', location: 'San Fransisco, California', followers: 40_378,
+                                   accepts_returns: true)
+
         @item_1 = Item.create!(name: 'Resin Sparkle Beer Cozy', rating: 2.2, price: 44.95,
-                              stock: 100, num_sold: 3, free_shipping: true)
+                               stock: 100, num_sold: 3, free_shipping: true,  artist_id: @artist_1.id)
         @item_2 = Item.create!(name: 'Boxer Bowler', rating: 5.0, price: 35.01,
-                              stock: 5, num_sold: 59, free_shipping: false)
+                               stock: 5, num_sold: 59, free_shipping: false, artist_id: @artist_2.id)
 
         visit '/items'
       end
@@ -29,8 +34,16 @@ RSpec.describe 'Item#index' do
         expect(page).to have_content(@item_2.stock)
         expect(page).to have_content(@item_1.num_sold)
         expect(page).to have_content(@item_2.num_sold)
-        expect(page).to have_content(@item_1.free_shipping)
-        expect(page).to have_content(@item_2.free_shipping)
+        if @item_1.free_shipping
+          expect(page).to have_content('Free!')
+        else
+          expect(page).to have_content('Paid')
+        end
+        if @item_2.free_shipping
+          expect(page).to have_content('Free!')
+        else
+          expect(page).to have_content('Paid')
+        end
       end
     end
   end
