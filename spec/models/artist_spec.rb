f708ff15d850b4f4838bbfb6539d8e69f8377f47
Astrid Hecht <silentaviary@gmail.com>
2022-08-29 17:50:01 -0600

    test: add tests to commit that were forgotten in it 1 (artist model tests)

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Artist do
+RSpec.configure do |config|
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+end
+
+RSpec.describe Artist, type: :model do
+  before :each do
+    @artist_1 = Artist.create!(username: 'UnnecessaryilyResin', location: 'Laramie, Wyoming', followers: 157,
+      accepts_returns: false)
+    @artist_2 = Artist.create!(username: 'PetHats', location: 'San Fransisco, California', followers: 40_378,
+      accepts_returns: true)
+    @item_1 = @artist_1.items.create!(name: 'Resin Sparkle Beer Cozy', rating: 2.2, price: 44.95,
+      stock: 100, num_sold: 3, free_shipping: true)
+    @item_2 = @artist_2.items.create!(name: 'Boxer Bowler', rating: 5.0, price: 35.01,
+      stock: 5, num_sold: 59, free_shipping: false)
+  end
   it {should have_many :items}
+
+  it 'sorts artist index by most recenetly created' do
+    @artists = Artist.recent
+    expect(@artists).to eq([@artist_2, @artist_1])
+  end
+
+  it 'can count items by artist' do
+    expect(@artist_1.tot_stock).to eq(1)
+  end
 end
