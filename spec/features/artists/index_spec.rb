cc9a178e2183e28bf83dbc3620428be488ac819c
Astrid Hecht <silentaviary@gmail.com>
2022-08-29 17:15:20 -0600

    test: Update artist index_spec for user story 17

--- spec/features/artists/index_spec.rb
+++ spec/features/artists/index_spec.rb
@@ -47,6 +47,16 @@ RSpec.describe 'Artists#index', type: :feature do
           expect(page.all('.artist')[1]).to have_content(@artist_1.created_at.to_s)
         end
       end
+
+      it 'every artist has an update button that takes you to edit page when clicked' do
+        expect(page).to have_button('Edit Artist')
+        expect(page.all('.artist')[0]).to have_button('Edit Artist')
+        expect(page.all('.artist')[1]).to have_button('Edit Artist') 
+        within(page.all('.artist')[0]) do
+          click_link('Edit Artist')
+        end
+        expect(current_path).to eq("/artists/#{@artist_2.id}/edit")
+      end
     end
   end
 end
